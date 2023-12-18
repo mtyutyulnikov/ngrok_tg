961ba6a1bc58ee8e0516948ff0493af862c8ac50
Mikhail Tyutyulnikov <64151232+mtyutyulnikov@users.noreply.github.com>
2023-12-18 19:39:46 +0700

    weather

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ app = Flask(__name__)
 
 
 def send_message(chat_id, text):
+    weather_url = "https://api.open-meteo.com/v1/forecast?latitude=55.0415&longitude=82.9346&current=temperature_2m&hourly=temperature_2m&timezone=Asia%2FBangkok&forecast_days=1"
+    weather_data = requests.get(weather_url).json()
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    text = f"Температура сейчас : {weather_data['current']['temperature_2m']} \nТемпература через 24 ч : {weather_data['hourly']['temperature_2m'][-1]}"
     requests.post(url, {"chat_id": chat_id, "text": text})
 
 
